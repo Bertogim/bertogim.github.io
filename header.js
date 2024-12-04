a95d7c9020c7ae5d5e94b7906303f8b1426a25b8
BuildTools <unconfigured@null.spigotmc.org>
2024-12-04 20:55:44 +0100

    Added video conversor and changed folder structure a bit

--- header.js
+++ header.js
@@ -26,10 +26,12 @@ link.addEventListener('load', () => {
                 <div class="dropdown">
                     <a href="/apps/video/" class="dropdown-toggle" es-translation="Herramientas de Video">Video Tools</a>
                     <div class="dropdown-menu">
-                        <a href="/apps/video/compressor" es-translation="Video Compress">Video Compress</a>
+                        <a href="/apps/video/compressor" es-translation="Compresor de Video">Video Compress</a>
+                        <a href="/apps/video/conversor" es-translation="Conversor de Video">Video Conversor</a>
+                        <a href="/apps/mp4-to-mp3" es-translation="MP4 a MP3">MP4 to MP3</a>
                     </div>
                 </div>
-                <a href="/apps/mp4-to-mp3" es-translation="MP4 a MP3">MP4 to MP3</a>
+                
             </div>
         </div>
 
